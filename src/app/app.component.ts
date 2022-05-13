c1cb6e0335d11c638dc1c1abda7c9acd58cbea7c
iamkd100 <srijan.khan20@gmail.com>
2022-05-13 21:16:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PaginationUsingAPI';
+  data: any;
+  page: number = 1;
+  itemsPerPage = 50;
+  totalItems: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllPassengerData()
+  }
+
+  getAllPassengerData() {
+    this.http.get(`https://jsonplaceholder.typicode.com/photos?page=${1}&size=${this.itemsPerPage}`).subscribe((data: any) => {
+
+      this.data = data
+      this.page = 0;
+      this.totalItems = data.length
+
+
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxPaginationModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
